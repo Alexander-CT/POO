5e0ce222eb73aa3def1f3008e7402eb6b33f5f2f
Alexander Castro Tocaffondi <40003942+Alexander-CT@users.noreply.github.com>
2019-04-25 11:58:50 -0500

    Laboratorio4

--- CASTRO_TOCAFFONDI/src/castro_tocaffondi/Trabajador.java
+++ CASTRO_TOCAFFONDI/src/castro_tocaffondi/Trabajador.java
@@ -12,7 +12,10 @@ package castro_tocaffondi;
 public class Trabajador {
     int dni;
     String nombre;
-    int sueldo;
+    float sueldo;
+    float AFP;
+    float ESSALUD;
+    float sueldofinal;
 
     public int getDni() {
         return dni;
@@ -30,13 +33,41 @@ public class Trabajador {
         this.nombre = nombre;
     }
 
-    public int getSueldo() {
+    public float getSueldo() {
         return sueldo;
     }
 
-    public void setSueldo(int sueldo) {
+    public void setSueldo(float sueldo) {
         this.sueldo = sueldo;
     }
+
+    public float getAFP() {
+        AFP=(float) (sueldo*0.1);
+        return AFP;
+    }
+
+    public void setAFP(float AFP) {
+        this.AFP = AFP;
+    }
+
+    public float getESSALUD() {
+        return ESSALUD;
+    }
+
+    public void setESSALUD(float ESSALUD) {
+        this.ESSALUD = ESSALUD;
+    }
+
+    public float getSueldofinal() {
+        sueldofinal=AFP+ESSALUD;
+        return sueldofinal;
+    }
+
+    public void setSueldofinal(float sueldofinal) {
+        this.sueldofinal = sueldofinal;
+    }
+    
+    
     
     
 }
